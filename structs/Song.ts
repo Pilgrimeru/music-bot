eed14ae64941b9ad86330d0ed6a1bd13eb44dfbd
Pilgrimeru <84448802+Pilgrimeru@users.noreply.github.com>
2022-09-19 16:29:54 +0200

    passage de ytdl à play-dl

--- structs/Song.ts
+++ structs/Song.ts
@@ -1,18 +1,16 @@
-import { AudioResource, createAudioResource, StreamType } from "@discordjs/voice";
-import youtube from "youtube-sr";
-import { getInfo } from "ytdl-core";
-import { getVideoID } from "ytdl-core";
-import ytdl from "ytdl-core-discord";
+import { AudioResource, createAudioResource } from "@discordjs/voice";
 import { i18n } from "../utils/i18n";
-import { videoPattern } from "../utils/patterns";
+import youtube from "youtube-sr";
+import { extractID, sp_validate, yt_validate } from "play-dl";
+const play = require('play-dl');
 
 export interface SongData {
   url: string;
   title: string;
   duration: number;
   id: string;
-
 }
+
 
 export class Song {
   public readonly url: string;
@@ -28,51 +26,65 @@ export class Song {
   }
 
   public static async from(url: string = "", search: string = "") {
-    const isYoutubeUrl = videoPattern.test(url);
-    // const isScUrl = scRegex.test(url);
 
     let songInfo;
+    if(url.startsWith('https') && sp_validate(url) === 'track') {
+      let sp_data = await play.spotify(url)
+      let result = await play.search(`${sp_data.name}`, {limit: 1})
+      songInfo = await play.video_info(result[0].url);
+      return new this({
+        url: result[0].url,
+        title: songInfo.video_details.title,
+        duration: songInfo.video_details.durationInSec,
+        id: extractID(result[0].url)
+      });
 
-    if (isYoutubeUrl) {
-      songInfo = await getInfo(url);
+    } else if (url.startsWith('https') && yt_validate(url) === 'video') {
+
+      songInfo = await play.video_info(url);
 
       return new this({
-        url: songInfo.videoDetails.video_url,
-        title: songInfo.videoDetails.title,
-        duration: parseInt(songInfo.videoDetails.lengthSeconds),
-        id: getVideoID(url)
+        url: url,
+        title: songInfo.video_details.title,
+        duration: songInfo.video_details.durationInSec,
+        id: extractID(url)
       });
     } else {
       const result = await youtube.searchOne(search);
 
-      songInfo = await getInfo(`https://youtube.com/watch?v=${result.id}`);
+
+      url = `https://youtube.com/watch?v=${result.id}`;
+
+      songInfo = await play.video_info(url);
 
       return new this({
-        url: songInfo.videoDetails.video_url,
-        title: songInfo.videoDetails.title,
-        duration: parseInt(songInfo.videoDetails.lengthSeconds),
-        id: getVideoID(`https://youtube.com/watch?v=${result.id}`)
+        url: url,
+        title: songInfo.video_details.title,
+        duration: songInfo.video_details.durationInSec,
+        id: extractID(url)
       });
     }
   }
 
   public async makeResource(): Promise<AudioResource<Song> | void> {
-    let stream;
-
-    let type = this.url.includes("youtube.com") ? StreamType.Opus : StreamType.OggOpus;
-
-    const source = this.url.includes("youtube") ? "youtube" : "soundcloud";
-
-    if (source === "youtube") {
-      stream = await ytdl(this.url, { quality: "highestaudio", highWaterMark: 1 << 25 });
+    
+    let source
+    if (this.url.startsWith('https') && yt_validate(this.url) === 'video') {
+      let info = await play.video_info(this.url)
+      const source = await play.stream_from_info(info)
+      return createAudioResource(source.stream, { metadata: this, inputType : source.type, inlineVolume: true });
     }
-
-    if (!stream) return;
-
-    return createAudioResource(stream, { metadata: this, inputType: type, inlineVolume: true });
+    if (this.url.startsWith('https') && yt_validate(this.url) === 'video') {
+      let info = await play.video_info(this.url)
+      const source = await play.stream_from_info(info)
+      return createAudioResource(source.stream, { metadata: this, inputType : source.type, inlineVolume: true });
+    }
+    
+    if (!source) return;
+    
   }
 
-  public startMessage() {
+  /*public startMessage() {
     return i18n.__mf("play.startedPlaying", { title: this.title, url: this.url });
-  }
+  }*/
 }
