e8fbc82f7e8f32d812b3b87bb23e50ceae102b36
Pilgrimeru <84448802+Pilgrimeru@users.noreply.github.com>
2022-09-19 22:50:36 +0200

    désactive mon début de spotify

--- structs/Song.ts
+++ structs/Song.ts
@@ -1,6 +1,6 @@
 import { AudioResource, createAudioResource } from "@discordjs/voice";
 import youtube from "youtube-sr";
-import { extractID, sp_validate, yt_validate } from "play-dl";
+import { extractID, yt_validate } from "play-dl";
 const play = require('play-dl');
 
 export interface SongData {
@@ -9,7 +9,6 @@ export interface SongData {
   duration: number;
   id: string;
 }
-
 
 export class Song {
   public readonly url: string;
@@ -27,7 +26,9 @@ export class Song {
   public static async from(url: string = "", search: string = "") {
 
     let songInfo;
-    if (url.startsWith('https') && sp_validate(url) === 'track') {
+
+    /* POUR SPOTIFY PLUS TARD
+      if (url.startsWith('https') && sp_validate(url) === 'track') {
       let sp_data = await play.spotify(url)
       let result = await play.search(`${sp_data.name}`, {limit: 1})
       songInfo = await play.video_info(result[0].url);
@@ -38,7 +39,8 @@ export class Song {
         id: extractID(result[0].url)
       });
 
-    } else if (url.startsWith('https') && yt_validate(url) === 'video') {
+    } else*/
+    if (url.startsWith('https') && yt_validate(url) === 'video') {
 
       songInfo = await play.video_info(url);
       
@@ -68,17 +70,11 @@ export class Song {
     
     let source
     if (this.url.startsWith('https') && yt_validate(this.url) === 'video') {
-      let info = await play.video_info(this.url)
-      const source = await play.stream_from_info(info)
-      return createAudioResource(source.stream, { metadata: this, inputType : source.type, inlineVolume: true });
-    }
-    if (this.url.startsWith('https') && yt_validate(this.url) === 'video') {
-      let info = await play.video_info(this.url)
+      let info = await play.video_info(this.url).catch(console.error);
       const source = await play.stream_from_info(info)
       return createAudioResource(source.stream, { metadata: this, inputType : source.type, inlineVolume: true });
     }
     
     if (!source) return;
-    
   }
 }
